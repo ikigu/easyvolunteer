0a6270908cb42d8b20a1fc113815e1057c1bfaba
Ikigu <ikigu.geo@gmail.com>
2024-06-19 12:20:21 +0300

    feat(apis): add method to update an organization

--- apis/organization_apis/organization.ts
+++ apis/organization_apis/organization.ts
@@ -72,4 +72,47 @@ router.post('/api/organizations', async (req, res, next) => {
     }
 });
 
+router.put('/api/organizations/:organizationId', async (req, res, next) => {
+    try {
+        const organization = await prisma.organization.findUnique({
+            where: {
+                id: req.params.organizationId
+            }
+        });
+
+        if (!organization) {
+            return res.status(404).json({ error: 'Organization not found' });
+        }
+    } catch (e) {
+        next(e);
+    }
+
+    if (Object.keys(req.body).length === 0) {
+        return res.status(400).json({ error: 'No items to update' });
+    }
+
+    const allowedFields = ['name', 'town', 'industry', 'description', 'userId'];
+
+    for (const key in req.body) {
+        if (!allowedFields.includes(key)) {
+            delete req.body[key];
+        }
+    }
+
+    req.body.updatedAt = new Date();
+
+    try {
+        const organization = await prisma.organization.update({
+            where: {
+                id: req.params.organizationId
+            },
+            data: req.body
+        });
+
+        res.json(organization);
+    } catch (e) {
+        next(e);
+    }
+});
+
 export default router;
