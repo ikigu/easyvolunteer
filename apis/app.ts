613c9c9f83a189c41a0392e3cb09b76259f0f1ad
Ikigu <ikigu.geo@gmail.com>
2024-06-19 11:11:39 +0300

    feat: register organization apis

--- apis/app.ts
+++ apis/app.ts
@@ -1,6 +1,7 @@
-import express from 'express';
-import userApis from './user_apis/user';
 import bodyParser from 'body-parser';
+import express from 'express';
+import organizationApis from './organization_apis/organization';
+import userApis from './user_apis/user';
 
 const app = express();
 
@@ -8,6 +9,7 @@ const port = process.env.API_PORT || 5001;
 
 app.use(bodyParser.json());
 app.use(userApis);
+app.use(organizationApis);
 
 app.listen(port, () => {
     console.log(`API listening on port ${port}`);
