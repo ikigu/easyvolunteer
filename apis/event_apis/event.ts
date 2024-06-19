4f76f1e2d7002869e3874b9f7ff6d9e31676c985
Ikigu <ikigu.geo@gmail.com>
2024-06-19 17:57:41 +0300

    feat(apis): add method to create a new event

--- apis/event_apis/event.ts
+++ apis/event_apis/event.ts
@@ -32,3 +32,75 @@ router.get('/api/events/:eventId', async (req, res, next) => {
         next(e);
     }
 });
+
+router.post('/api/events', async (req, res, next) => {
+    // Check if all required fields have been assigned
+    const requiredFields = [
+        'creatorId',
+        'organizationId',
+        'title',
+        'startTime',
+        'endTime',
+        'cost'
+    ];
+
+    for (const field of requiredFields) {
+        if (!req.body[field]) {
+            return res
+                .status(400)
+                .json({ error: `Required field not provided: ${field}` });
+        }
+    }
+
+    // Check if user exists
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: req.body.creatorId
+            },
+            include: {
+                organizations: true
+            }
+        });
+
+        if (!user) return res.status(404).json({ error: 'User not found' });
+
+        let organizationExists = false;
+
+        for (const organization of user.organizations) {
+            if (organization.id === req.body.organizationId) {
+                organizationExists = true;
+            }
+        }
+
+        if (!organizationExists)
+            return res.status(404).json({ error: 'Organization not found' });
+    } catch (e) {
+        next(e);
+    }
+
+    // Check if org exists
+
+    // Delete unallowed fields
+
+    for (const key in req.body) {
+        if (!requiredFields.includes(key)) {
+            delete req.body[key];
+        }
+    }
+
+    // Create the event
+
+    try {
+        const event = await prisma.event.create({
+            data: req.body
+        });
+
+        return res.json(event);
+    } catch (e) {
+        next(e);
+    }
+});
+
+export default router;
