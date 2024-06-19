ba3ecef2b3b2d998d68b80e68465496a07941fc1
Ikigu <ikigu.geo@gmail.com>
2024-06-19 10:07:26 +0300

    feat(apis): add ability to parse json request bodies

--- apis/app.ts
+++ apis/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import userApis from './user_apis/user';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 const port = process.env.API_PORT || 5001;
 
+app.use(bodyParser.json());
 app.use(userApis);
 
 app.listen(port, () => {
