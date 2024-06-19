b84f29157232ae66f3ff48c7c80db59b5a613f1f
Ikigu <ikigu.geo@gmail.com>
2024-06-19 14:18:51 +0300

    feat(apis): add method to return a single event

--- apis/event_apis/event.ts
+++ apis/event_apis/event.ts
@@ -14,3 +14,21 @@ router.get('api/events', async (req, res, next) => {
         next(e);
     }
 });
+
+router.get('/api/events/:eventId', async (req, res, next) => {
+    // Return event of eventId if it exists
+
+    try {
+        const event = await prisma.event.findUnique({
+            where: {
+                id: req.params.eventId
+            }
+        });
+
+        if (event) return res.json(event);
+
+        return res.status(404).json({ error: 'Not found' });
+    } catch (e) {
+        next(e);
+    }
+});
