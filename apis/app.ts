522afeaa2dd7eb20376cf2af755d1ec20a0e8a35
Ikigu <ikigu.geo@gmail.com>
2024-06-20 10:52:52 +0300

    feat(api): register participantRoles api routes for usage by api app

--- apis/app.ts
+++ apis/app.ts
@@ -2,6 +2,7 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import eventApis from './event_apis/event';
 import organizationApis from './organization_apis/organization';
+import participantRoleApis from './participant_role_apis/participant_roles';
 import userApis from './user_apis/user';
 
 const app = express();
@@ -15,6 +16,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 app.use(eventApis);
 app.use(organizationApis);
+app.use(participantRoleApis);
 app.use(userApis);
 
 app.listen(port, () => {
