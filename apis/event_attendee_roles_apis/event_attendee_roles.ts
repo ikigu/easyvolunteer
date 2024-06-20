daddf31e568dbb8539a1a4a3d3c335a330a3ddb1
Ikigu <ikigu.geo@gmail.com>
2024-06-20 11:57:28 +0300

    feat(apis): add route to update a role for event attendees

--- apis/event_attendee_roles_apis/event_attendee_roles.ts
+++ apis/event_attendee_roles_apis/event_attendee_roles.ts
@@ -50,4 +50,61 @@ router.get('/api/:roleType/:roleId', async (req, res, next) => {
     }
 });
 
+router.put('/api/:roleType/:roleId', async (req, res, next) => {
+    // Updates either a volunteer role or a participant role of given roleId
+
+    const allowedRoleTypesInReqPath = ['participant_roles', 'volunteer_roles'];
+
+    if (!allowedRoleTypesInReqPath.includes(req.params.roleType)) {
+        return res.status(404).json({ error: 'Not found' });
+    }
+
+    const roleType =
+        req.params.roleType === 'volunteer_roles'
+            ? 'volunteerRole'
+            : 'participantRole';
+
+    const allowedFields = ['title', 'description'];
+
+    for (const key in req.body) {
+        if (!allowedFields.includes(key)) {
+            delete req.body[key];
+        }
+    }
+
+    req.body.updatedAt = new Date();
+
+    try {
+        if (roleType === 'volunteerRole') {
+            const volunteerRole = await prisma[roleType].update({
+                where: {
+                    id: req.params.roleId
+                },
+                data: req.body
+            });
+
+            return res.json(volunteerRole);
+        } else {
+            const participantRole = await prisma[roleType].update({
+                where: {
+                    id: req.params.roleId
+                },
+                data: req.body
+            });
+
+            return res.json(participantRole);
+        }
+    } catch (e) {
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+            if (e.code === 'P2025') {
+                return res
+                    .status(404)
+                    .json({ error: 'Resource to update not found' });
+            }
+        }
+
+        next(e);
+    }
+});
+
 export default router;
